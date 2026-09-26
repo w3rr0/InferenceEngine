1560064fa173889d246ba09c146ef355c17eebc6
w3rr0 <konradmateja65@gmail.com>
2026-09-26 16:47:49 +0200

    use engine runner in main entrypoint

--- inference_engine/__main__.py
+++ inference_engine/__main__.py
@@ -1,4 +1,13 @@
-from inference_engine.engine_core import Engine
+from inference_engine.runner import EngineRunner
 
-engine = Engine(1024, 16, 4)
-print(f"Engine running: {engine}")
+def main():
+    runner = EngineRunner(total_blocks=128, block_size=16, max_batch_size=4)
+
+    runner.add_prompt("The capital of France is")
+    runner.add_prompt("Once upon a time in a galaxy")
+
+    # TODO: without new block allocation during decoding, the total length (prompt + num_steps) cannot yet exceed block_size num of tokens
+    runner.run_steps(num_steps=8)
+
+if __name__ == "__main__":
+    main()
